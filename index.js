482b877bc31c06ced086af25cb4c4f6ca84b76fe
Robert Frazier <robert.v.frazier@gmail.com>
2017-11-08 16:45:07 -0500

    Basic functionality is working! Stretch goals?

--- index.js
+++ index.js
@@ -18,47 +18,68 @@
 /******************************************************** 
  All global variables here. 
 ********************************************************/
-// const STORE = {
-//     results {
+const STORE = {
+  endpoint: 'https://www.googleapis.com/youtube/v3/search',
+  searchString: ''
 
-//     }
-// }
-
-// const query = {
-//     q: $(),
-//     part:,
-//     key:
-// }
+};
 
 /******************************************************** 
 Step 1: Render the DOM. 
 ********************************************************/
 
+function buildJSON(searchString) {
+  // Recieves the searchString and builds the .getJSON request
+  console.log('In buildJSON() function');
+  const query = {
+    q: `${searchString} in:name`,
+    key: 'AIzaSyBTVA9IpirPat_W80y0Abl5N9qoaXOTlq0',
+    part: 'snippet'
+  };
+  $.getJSON(STORE.endpoint, query, getJSONResponse);  // getJSONResponse is the callback function.
+}
+
+function getJSONResponse(data){
+  console.log('In getJSONResponse() function');
+  renderJSONResults(data);
+}
+
+function renderJSONResults(results){
+  console.log('In renderJSONResults() function');
+  console.log(results);
+  const jsonHTML=results.items.map(function(item){
+    const thumbnail=item.snippet.thumbnails.default.url;
+    const height=item.snippet.thumbnails.default.height;
+    const width=item.snippet.thumbnails.default.width;
+    const videoURL = `https://youtu.be/${item.id.videoId}`;
+    const altText = item.snippet.title;
+    console.log(videoURL);
+    return `
+    <a href='${videoURL}' target='_blank'><img src='${thumbnail}' alt='${altText}' height='${height}' width='${width}'>
+    `;
+  });
+
+  $('.displayResults').html(jsonHTML);
+}
 
 
 /******************************************************** 
  * Step 2: Listen for user interactions.
  ********************************************************/
-//function that listens to button inputRobert Frazier9:31 AM
+//function that listens to button input
 function addListeners() {
-  console.log('add addlisteners works!!');
+  console.log('In addlisteners() function');
   $('#js-searchForm').on('submit', (event) => { 
     event.preventDefault();
-    const searchString = $('.js-search-entry').val(); 
-    console.log(searchString); 
+    STORE.searchString = $('.js-search-entry').val(); 
+    console.log(STORE.searchString);
+    buildJSON(STORE.searchString);
   });
-  $('.js-searchButton').on('click', 'button', () => { 
-  }); 
 }
-
-
-
 
 /******************************************************** 
  * Step 3: Change the state of the STORE. 
  ********************************************************/
-
-
 
 
 
